59d6d12932a84b43233f8ea762afe4f2ed349e88
Andre Herbst <moormaster@gmx.net>
2023-12-03 16:46:15 +0100

    add solution of day1 part 2

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -2,13 +2,36 @@ use adventofcode_2023::input_helper;
 
 fn to_calibration_value(line : &str) -> Result<u8, &str> {
     let mut first_digit: Option<u8> = None;
-    let mut last_digit: Option<u8> = None;;
+    let mut last_digit: Option<u8> = None;
 
-    for char in line.chars() {
+    for index_char in line.char_indices() {
+        let index = index_char.0;
+        let char = index_char.1;
+
         let mut digit: Option<u8> = None;
         
         if char.is_digit(10) {
             digit = Some(char.to_digit(10).unwrap() as u8);
+        }
+
+        if line[index..].starts_with("one") {
+            digit = Some(1);
+        } else if line[index..].starts_with("two") {
+            digit = Some(2);
+        } else if line[index..].starts_with("three") {
+            digit = Some(3);
+        } else if line[index..].starts_with("four") {
+            digit = Some(4);
+        } else if line[index..].starts_with("five") {
+            digit = Some(5);
+        } else if line[index..].starts_with("six") {
+            digit = Some(6);
+        } else if line[index..].starts_with("seven") {
+            digit = Some(7);
+        } else if line[index..].starts_with("eight") {
+            digit = Some(8);
+        } else if line[index..].starts_with("nine") {
+            digit = Some(9);
         }
 
         if let Some(digit) = digit {
@@ -58,6 +81,11 @@ mod tests {
         #[test]
         fn it_concatenates_first_digit_with_last_digit() {
             assert_eq!(to_calibration_value("word1word2word3word"), Ok(13))
+        }
+
+        #[test]
+        fn it_recognizes_words_as_digit() {
+            assert_eq!(to_calibration_value("one2three"), Ok(13))
         }
     }
 }
