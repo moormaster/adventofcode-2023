19f7388e99455dde2539ea9e07e6aefe69c4f38d
Andre Herbst <moormaster@gmx.net>
2023-12-03 19:50:27 +0100

    refactor: extracted solution for part1 into seperate method

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,6 +1,33 @@
 use adventofcode_2023::input_helper;
 
-fn to_calibration_value(line : &str) -> Result<u8, &str> {
+fn process_line_part1(line : &str) -> Result<u8, &str> {
+    let mut first_digit: Option<u8> = None;
+    let mut last_digit: Option<u8> = None;
+
+    for char in line.chars() {
+        let mut digit: Option<u8> = None;
+        
+        if char.is_digit(10) {
+            digit = Some(char.to_digit(10).unwrap() as u8);
+        }
+
+        if let Some(digit) = digit {
+            if first_digit == None {
+                first_digit = Some(digit);
+            }
+            
+            last_digit = Some(digit);
+        }
+    }
+
+    if let Some(first_digit) = first_digit {
+        Ok(first_digit*10 + last_digit.unwrap())
+    } else {
+        Err("no digits")
+    }
+}
+
+fn process_line_part2(line : &str) -> Result<u8, &str> {
     let mut first_digit: Option<u8> = None;
     let mut last_digit: Option<u8> = None;
 
@@ -51,40 +78,66 @@ fn to_calibration_value(line : &str) -> Result<u8, &str> {
 
 pub fn main() -> std::io::Result<()> {
     let lines = input_helper::read_lines("input/day01")?;
+    let sum_part1: u32 = 
+    lines.into_iter()
+        .map(|line| { process_line_part1(&line.unwrap()).unwrap() as u32 } )
+        .sum();
 
-    let mut sum = 0u32;
-    for line in lines {
-        sum += to_calibration_value(&line?).unwrap() as u32;
-    }
+    println!("part1: {sum_part1}");
 
-    println!("{sum}");
+    let lines = input_helper::read_lines("input/day01")?;
+    let sum_part2: u32 = 
+        lines.into_iter()
+            .map(|line| { process_line_part2(&line.unwrap()).unwrap() as u32 } )
+            .sum();
+
+    println!("part2: {sum_part2}");
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    mod to_calibration_value {
-        use crate::to_calibration_value;
+    mod process_line_part1 {
+        use crate::process_line_part1;
 
         #[test]
         fn it_fails_without_any_digits() {
-            assert_eq!(to_calibration_value("aWord"), Err("no digits"))
+            assert_eq!(process_line_part1("aWord"), Err("no digits"))
         }
 
         #[test]
         fn it_concatenates_the_only_digit_twice() {
-            assert_eq!(to_calibration_value("word1word"), Ok(11))
+            assert_eq!(process_line_part1("word1word"), Ok(11))
         }
 
         #[test]
         fn it_concatenates_first_digit_with_last_digit() {
-            assert_eq!(to_calibration_value("word1word2word3word"), Ok(13))
+            assert_eq!(process_line_part1("word1word2word3word"), Ok(13))
+        }
+    }
+
+    mod process_line_part2 {
+        use crate::process_line_part2;
+
+        #[test]
+        fn it_fails_without_any_digits() {
+            assert_eq!(process_line_part2("aWord"), Err("no digits"))
+        }
+
+        #[test]
+        fn it_concatenates_the_only_digit_twice() {
+            assert_eq!(process_line_part2("word1word"), Ok(11))
+        }
+
+        #[test]
+        fn it_concatenates_first_digit_with_last_digit() {
+            assert_eq!(process_line_part2("word1word2word3word"), Ok(13))
         }
 
         #[test]
         fn it_recognizes_words_as_digit() {
-            assert_eq!(to_calibration_value("one2three"), Ok(13))
+            assert_eq!(process_line_part2("one2three"), Ok(13))
         }
     }
 }
