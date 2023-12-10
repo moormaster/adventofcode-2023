c7105205e4398ae980ae78e173f9faa1a4125a74
Andre Herbst <moormaster@gmx.net>
2023-12-10 15:37:39 +0100

    add solution for day03 part 2

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -25,6 +25,7 @@ fn main() -> io::Result<()>{
     let items_from_second_line = parse_line(&lines.next().unwrap()?)?;
 
     let mut part_numbers = vec![];
+    let mut wheels = vec![];
 
     let mut items_above: Option<Vec<ParsedItem>> = None;
     let mut items = Some(items_from_first_line);
@@ -32,6 +33,7 @@ fn main() -> io::Result<()>{
 
     while items.is_some() {
         part_numbers.append(&mut get_part_numbers(items_above.as_ref(), items.as_ref().unwrap(), items_below.as_ref())?);
+        wheels.append(&mut get_wheels(items_above.as_ref(), items.as_ref().unwrap(), items_below.as_ref())?);
 
         items_above = items;
         items = items_below;
@@ -41,6 +43,12 @@ fn main() -> io::Result<()>{
     }
 
     println!("Part 1: {}", part_numbers.into_iter().sum::<u32>());
+    println!(
+        "Part 2: {}", 
+        wheels.into_iter()
+            .map(|wheel| {wheel.0 * wheel.1})
+            .sum::<u32>()
+    );
 
     Ok(())
 }
@@ -112,6 +120,72 @@ fn get_part_numbers(items_above: Option<&Vec<ParsedItem>>, items: &Vec<ParsedIte
     }
 
     Ok(values)
+}
+
+fn get_wheels(items_above: Option<&Vec<ParsedItem>>, items: &Vec<ParsedItem>, items_below: Option<&Vec<ParsedItem>>) -> io::Result<Vec<(u32, u32)>> {
+    let mut wheels: Vec<(u32, u32)> = vec![];
+    let mut items = items.iter();
+
+    let mut item_before: Option<&ParsedItem> = None;
+    let mut item = items.next();
+    let mut item_behind = items.next();
+
+    while item.is_some() {
+        let mut adjacent_numbers = vec![0u32; 0];
+
+        if let Some(item) = item {
+            if let AstItem::Symbol { value: _ } = item.value {
+                let surrounding_range = get_surrounding_range(&item.range);
+
+                // find adjacent numbers in line above
+                if let Some(items_above) = items_above {
+                    for item_above in items_above {
+                        if let AstItem::Number { value } = item_above.value {
+                            if surrounding_range.contains(&item_above.range.start) || surrounding_range.contains(&(item_above.range.end-1)) {
+                                adjacent_numbers.push(value);
+                            }
+                        }
+                    }
+                }
+
+                // check for adjacent numbers in current line
+                if let Some(item_before) = item_before {
+                    if let AstItem::Number { value } = item_before.value {
+                        adjacent_numbers.push(value);
+                    }
+                }
+
+                if let Some(item_behind) = item_behind {
+                    if let AstItem::Number { value } = item_behind.value {
+                        adjacent_numbers.push(value);
+                    }
+                }
+
+                // find adjacent numbers in line below
+                if let Some(items_below) = items_below {
+                    for item_below in items_below {
+                        if let AstItem::Number { value } = item_below.value {
+                            if surrounding_range.contains(&item_below.range.start) || surrounding_range.contains(&(item_below.range.end-1)) {
+                                adjacent_numbers.push(value);
+                            }
+                        }
+                    }
+                }
+
+                if adjacent_numbers.len() == 2 {
+                    wheels.push((adjacent_numbers[0], adjacent_numbers[1]));
+                }
+            }
+        }
+
+        // advance variables for item before, current item, item behind
+        item_before = item;
+        item = item_behind;
+        // ... and read next item in line
+        item_behind = items.next();
+    }
+
+    Ok(wheels)
 }
 
 fn is_symbol(c: char) -> bool {
@@ -307,6 +381,68 @@ mod test
                 ).unwrap(),
                 "multiple lines with some part numbers"
             );
+        }
+
+        mod get_wheels {
+            use crate::{get_wheels, parse_line};
+
+            #[test]
+            fn it_returns_a_single_wheel() {
+                assert_eq!(
+                    vec![
+                        (23, 42)
+                    ],
+
+                    get_wheels(
+                        None,
+                        &parse_line("23*42").unwrap(),
+                        None
+                    ).unwrap(),
+                    "single wheel within the same line"
+                );
+
+                assert_eq!(
+                    vec![
+                        (42, 123)
+                    ],
+
+                    get_wheels(
+                        Some(   &parse_line("42....").unwrap()),
+                                &parse_line("..*...").unwrap(),
+                        Some(   &parse_line("...123").unwrap())
+                    ).unwrap(),
+                    "single wheel with upper left and lower right number"
+                );
+
+                assert_eq!(
+                    vec![
+                        (123, 42)
+                    ],
+
+                    get_wheels(
+                        Some(   &parse_line("...123").unwrap()),
+                                &parse_line("..*...").unwrap(),
+                        Some(   &parse_line("42....").unwrap())
+                    ).unwrap(),
+                    "single wheel with upper right and lower left number"
+                );
+            }
+
+            #[test]
+            fn it_returns_multiple_wheels() {
+                assert_eq!(
+                    vec![
+                        (42, 123)
+                    ],
+
+                    get_wheels(
+                        Some(   &parse_line("1..42.....6").unwrap()),
+                                &parse_line(".*...*....*").unwrap(),
+                        Some(   &parse_line("2.3...123..").unwrap())
+                    ).unwrap(),
+                    "wheel enclosed in non-wheels"
+                );
+            }
         }
     }
 }
