c21e483aab5796a56cb28a41f15a3c973ffdc2ad
moormaster <moormaster@gmx.net>
2023-12-25 17:46:47 +0100

    refactor: expand distances instead of expanding the universe

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -9,32 +9,38 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn process(lines: &Vec<String>) -> usize {
-    let expanded_universe = expand(&lines);
-    let galaxies = location_of_galaxies(&expanded_universe);
+fn process(lines: &Vec<String>, expand_times: usize) -> usize {
+    let galaxies = location_of_galaxies(&lines);
+
+    let rows_to_expand = get_rows_to_expand(lines);
+    let columns_to_expand = get_columns_to_expand(lines);
 
     galaxies.iter().enumerate()
         .map(|(index_g1, g1)|
 
         galaxies.iter().enumerate()
             .filter(|(index_g2, _)| index_g1 < *index_g2)
-            .map(|(_, g2)| distance(*g1, *g2) )
+            .map(|(_, g2)| distance(*g1, *g2, &rows_to_expand, &columns_to_expand, expand_times) )
             .collect::<Vec<usize>>()
         )
         .flatten()
         .sum::<usize>()
 }
 
-fn expand(lines: &Vec<String>) -> Vec<String> {
-    let mut expanded_lines = lines.clone();
+fn get_rows_to_expand(lines: &Vec<String>) -> Vec<usize> {
+    let mut rows_to_expand = Vec::new();
 
     // expand rows
     for (index, row) in lines.iter().enumerate().rev() {
         if row.chars().all(|e| e == '.') {
-            expanded_lines.insert(index, row.clone());
+            rows_to_expand.push(index)
         }
     }
 
+    rows_to_expand
+}
+
+fn get_columns_to_expand(lines: &Vec<String>) -> Vec<usize> {
     // expand columns
     let mut columns_to_expand = Vec::new();
     for (index, _) in lines[0].chars().enumerate() {
@@ -46,13 +52,7 @@ fn expand(lines: &Vec<String>) -> Vec<String> {
         }
     }
 
-    for index in columns_to_expand.iter().rev() {
-        for line in expanded_lines.iter_mut() {
-            line.insert(*index, '.');
-        }
-    }
-
-    expanded_lines
+    columns_to_expand
 }
 
 fn location_of_galaxies(universe: &Vec<String>) -> Vec<(usize, usize)> {
@@ -67,11 +67,25 @@ fn location_of_galaxies(universe: &Vec<String>) -> Vec<(usize, usize)> {
         .collect()
 }
 
-fn distance(g1: (usize, usize), g2: (usize, usize)) -> usize {
+fn distance(g1: (usize, usize), g2: (usize, usize), rows_to_expand: &Vec<usize>, columns_to_expand: &Vec<usize>, expand_times: usize) -> usize {
     let vertical_distance = if g2.1 > g1.1 { g2.1 - g1.1 } else { g1.1 - g2.1 };
     let horizontal_distance = if g2.0 > g1.0 { g2.0 - g1.0 } else { g1.0 - g2.0 };
 
-    vertical_distance + horizontal_distance
+    let vertical_expansion: usize =
+        rows_to_expand.iter()
+            .filter(|row| if g2.1 > g1.1 { **row > g1.1 } else { **row > g2.1 })
+            .filter(|row| if g2.1 > g1.1 { **row < g2.1 } else { **row < g1.1 })
+            .map(|_| expand_times-1)
+            .sum();
+
+    let horizontal_expansion: usize =
+        columns_to_expand.iter()
+            .filter(|column| if g2.0 > g1.0 { **column > g1.0 } else { **column > g2.0 })
+            .filter(|column| if g2.0 > g1.0 { **column < g2.0 } else { **column < g1.0 })
+            .map(|_| expand_times-1)
+            .sum();
+
+    vertical_distance + vertical_expansion + horizontal_distance + horizontal_expansion
 }
 
 #[cfg(test)]
@@ -100,7 +114,9 @@ mod test {
                     &SAMPLE_INPUT_1
                         .lines()
                         .map(|e| e.to_string())
-                        .collect()));
+                        .collect(),
+                        
+                    2));
         }
     }
 }
